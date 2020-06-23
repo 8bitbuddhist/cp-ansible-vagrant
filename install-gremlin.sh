c5ba133f0c1e65df48822953ba33ce53e94d195e
Andre Newman <10094408+8bitbuddhist@users.noreply.github.com>
2020-06-23 11:27:37 -0400

    Fixed Gremlin config breaking on subsequent `vagrant up`

--- install-gremlin.sh
+++ install-gremlin.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 sudo curl https://rpm.gremlin.com/gremlin.repo -o /etc/yum.repos.d/gremlin.repo
 sudo yum install -y gremlin gremlind
-printf "\nidentifier: $1" >> /etc/gremlin/config.yaml
-printf "\nteam_id: $GREMLIN_TEAM_ID\n\ntags:\n  service: kafka\n\n" >> /etc/gremlin/config.yaml
-if [ -n "$GREMLIN_TEAM_SECRET" ]; then
-	printf "team_secret: $GREMLIN_TEAM_SECRET\n" >> /etc/gremlin/config.yaml
-else
-	printf "team_certificate: file:///gremlin/gremlin.cert\nteam_private_key: file:///gremlin/gremlin.key\n" >> /etc/gremlin/config.yaml
+
+if ! grep -q "identifier: $1" /etc/gremlin/config.yaml; then
+	printf "\nidentifier: $1" >> /etc/gremlin/config.yaml
+	printf "\nteam_id: $GREMLIN_TEAM_ID\n\ntags:\n  service: kafka\n\n" >> /etc/gremlin/config.yaml
+	if [ -n "$GREMLIN_TEAM_SECRET" ]; then
+		printf "team_secret: $GREMLIN_TEAM_SECRET\n" >> /etc/gremlin/config.yaml
+	else
+		printf "team_certificate: file:///gremlin/gremlin.cert\nteam_private_key: file:///gremlin/gremlin.key\n" >> /etc/gremlin/config.yaml
+	fi
 fi
 
 sudo service gremlind restart
